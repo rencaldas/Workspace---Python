a92c982066ea420d9a975db4e137a053b704e210
Renato Caldas <renatodeacaldas@gmail.com>
2025-09-09 01:16:25 -0300

    "shallow_copy_dict.py" atualizado para praticar mais

--- shallow_copy_dict.py
+++ shallow_copy_dict.py
@@ -9,6 +9,29 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
+p1 = {
+    'nome': 'Renato',
+    'sobrenome': 'Caldas',
+}
+# print(p1['nome'])
+# print(p1.get('nome', 'Não existe'))
+
+# nome = p1.pop('nome')
+# print(nome)
+# print(p1)
+# ultima_chave = p1.popitem()
+# print(ultima_chave)
+# print(p1)
+# p1.update({
+#     'nome': 'novo valor',
+#     'idade': 30,
+# })
+# p1.update(nome='novo valor', idade=30)
+# tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
+
 import copy
 
 d1 = {
