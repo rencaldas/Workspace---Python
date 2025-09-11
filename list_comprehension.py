0300211e09daeadde06bdcbdd650c15bba24025d
Renato Caldas <renato.deacaldas@gmail.com>
2025-09-10 22:31:58 -0300

    Teste

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,5 +1,5 @@
 # Introdução à List comprehension em Python
-# List comprehension é uma forma rápida para criar listas
+# List comprehension é uma forma rápida para criar listas.
 # a partir de iteráveis.
 # print(list(range(10)))
 import pprint
