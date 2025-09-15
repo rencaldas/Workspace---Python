5fc9d4e8c3083ddb94b1517e6ba036922ebbff09
Renato Caldas <renato.deacaldas@gmail.com>
2025-09-15 05:58:42 -0300

    Testado isintance()

--- list_comprehension2.py
+++ list_comprehension2.py
@@ -1,15 +1,36 @@
-lista = []
-for x in range(3):
-    for y in range(3):
-        lista.append((x, y))
-lista = [
-    (x, y)
-    for x in range(3)
-    for y in range(3)
-]
-lista = [
-    [(x, letra) for letra in 'Luiz']
-    for x in range(3)
-]
+# Funções
+def gerar_lista(qtd, mult):
+    # Gera a lista usando list comprehension
+    return [n * mult for n in range(qtd)]
 
-print(lista)
+def mostrar_resultados(lista, mult):
+    print("\n--- RESULTADOS ---")
+    [print(f"{i} * {mult} = {valor}") for i, valor in enumerate(lista)]
+
+def mostrar_pares(lista):
+    pares = [n for n in lista if n % 2 == 0]
+    print(f"\nNúmeros pares: {pares}")
+
+def mostrar_impares(lista):
+    impares = [n for n in lista if n % 2 != 0]
+    print(f"\nNúmeros ímpares: {impares}")
+
+def estatisticas(lista):
+    maior = max(lista)
+    menor = min(lista)
+    soma = sum(lista)
+    print("\n--- ESTATÍSTICAS ---")
+    print(f"Maior valor: {maior}")
+    print(f"Menor valor: {menor}")
+    print(f"Soma total: {soma}")
+
+# Programa principal
+numeros = int(input("Quantos números você quer criar? "))
+multiplicar = int(input("Por quanto deseja multiplicar? "))
+
+lista = gerar_lista(numeros, multiplicar)
+
+mostrar_resultados(lista, multiplicar)
+mostrar_pares(lista)
+mostrar_impares(lista)
+estatisticas(lista)
