f62cb901bffdff90e86834201e2dd3d784114423
Renato Caldas <renato.deacaldas@gmail.com>
2025-09-16 09:47:18 -0300

    raise, import, try e except

--- try_e_except.py
+++ try_e_except.py
@@ -1,22 +1,17 @@
-# (Parte1) try e except para tratar exceções
-# a = 18
-# b = 0
-# c = a / b
-
+# try, except, else e finally
+# https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
 try:
-    a = 18
-    b = 0
-    # print(b[0])
-    print('Linha 1'[1000])
-    c = a / b
-    print('Linha 2')
-except ZeroDivisionError:
-    print('Dividiu por zero.')
-except NameError:
-    print('Nome b não está definido')
-except (TypeError, IndexError):
-    print('TypeError + IndexError')
-except Exception:
-    print('ERRO DESCONHECIDO.')
-
-print('CONTINUAR')
+    print('ABRIR ARQUIVO')
+    8/0
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
+    print('DIVIDIU ZERO')
+except IndexError as error:
+    print('IndexError')
+except (NameError, ImportError):
+    print('NameError, ImportError')
+else:
+    print('Não deu erro')
+finally:
+    print('FECHAR ARQUIVO')
